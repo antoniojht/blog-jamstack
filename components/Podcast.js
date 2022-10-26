597319b45318608f2e58841f95b746e30ae636e0
Antonio Jose <ajherreratabaco@gmail.com>
2022-10-26 13:11:38 +0200

    blog next following carlos azaustres tutorial

---
+++ components/Podcast.js
@@ -0,0 +1,11 @@
+export const Podcast = ({episode}) => {
+  return (
+    <iframe
+      src={`https://open.spotify.com/embed/playlist/5a2OuIJ1kEttA8X3PaewlI?utm_source=oembed`}
+      width="100%"
+      height="232"
+      frameBorder="0"
+      allow="encrypted-media"
+    ></iframe>
+  )
+}
